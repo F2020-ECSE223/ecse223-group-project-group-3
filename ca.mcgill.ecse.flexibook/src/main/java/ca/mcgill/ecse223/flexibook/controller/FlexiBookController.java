8b71181c3cb532de3643574ae0d9c0ae28a61077
RobertApra <robert.aprahamian@mail.mcgill.ca>
2020-10-19 23:35:28 +0200

    Started working on my methods

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse223/flexibook/controller/FlexiBookController.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse223/flexibook/controller/FlexiBookController.java
@@ -1,9 +1,14 @@
-package ca.mcgill.ecse223.flexibook.controller;
+	package ca.mcgill.ecse223.flexibook.controller;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
-
+import ca.mcgill.ecse.flexibook.model.Appointment;
 import ca.mcgill.ecse.flexibook.application.FlexiBookApplication;
+import ca.mcgill.ecse.flexibook.application.Service;
+import ca.mcgill.ecse.flexibook.application.ServiceCombo;
+import ca.mcgill.ecse.flexibook.model.BookableService;
 import ca.mcgill.ecse.flexibook.model.Customer;
 import ca.mcgill.ecse.flexibook.model.Owner;
 import ca.mcgill.ecse.flexibook.model.User;
@@ -73,7 +78,55 @@ public class FlexiBookController {
 		
 		return foundUser;
 	}
+//---------------------------------------------------------------------------------------------------------------------
+	// Robert code for service combo
 	
+	// 1. Define ServiceCombo 
+	public static void defineServiceCombo(BookableService BkService, String SCname, String mainService, String services, String mandatory) {
+		
+		
+		FlexiBookApplication.getFlexibook().addBookableService(BkService);
+	}
+	
+	// 2. Update ServiceCombo
+	public static void updateServiceCombo(String SCname) {
+		
+	}
+	
+	// 3. Delete ServiceCombo
+	
+	public static void deleteServiceCombo(String SCname) {
+		if (FlexiBookApplication.getCurrentUser() != FlexiBookApplication.getFlexibook().getOwner()) {
+			//throw new error
+		}
+		else {
+		BookableService sc = findServiceCombo(SCname);
+		if (sc != null) {
+			sc.delete();
+		}
+		else {
+			throw new RuntimeException("There is no Service combo with this name");
+		}
+		List<Appointment> a = FlexiBookApplication.getFlexibook().getAppointments();
+		for (Appointment app : a) {
+			if (app.getBookableService().getName() == SCname) {
+			app.delete();
+			}
+		}
+		}
+	}
+	
+	private static BookableService findServiceCombo(String name) {
+		BookableService SCfound = null;
+		for (BookableService Sc : FlexiBookApplication.getFlexibook().getBookableServices()) {
+			if (Sc.getName() == name ) {
+				SCfound = Sc;
+			}
+		}
+		return SCfound;
+	}
+	
+//---------------------------------------------------------------------------------------------------------------------
 	private static boolean checkPassword(User user, String password) {
 		if (user.getPassword().equals(password)) return true;
 		
