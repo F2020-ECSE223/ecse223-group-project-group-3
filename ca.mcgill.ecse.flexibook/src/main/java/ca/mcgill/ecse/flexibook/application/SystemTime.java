43c0efa124338a080a8ecc505aaa606bb900d249
ericchehata <70850294+ericchehata@users.noreply.github.com>
2020-10-24 17:45:29 -0400

    solving merge conflicts

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/application/SystemTime.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/application/SystemTime.java
@@ -7,19 +7,15 @@ public class SystemTime {
 
 	private static Date sysDate = null;
 	private static Time sysTime = null;
-	
+
 	public static Date getSysDate() {
 		return sysDate;
 	}
-	
+
 	public static Time getSysTime() {
 		return sysTime;
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> Tamara
 	public static void setSysDate(Date date) {
 		sysDate = date;
 	}
@@ -27,10 +23,4 @@ public class SystemTime {
 	public static void setSysTime(Time time) {
 		sysTime = time;
 	}
-<<<<<<< HEAD
-   
 }
-
-=======
-}
->>>>>>> Tamara
